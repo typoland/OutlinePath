7d3d72f1fd3e05860d56b5be69e247ba75959efd
Łukasz Dziedzic <dziedzic@alfabety.pl>
2021-06-26 20:43:33 +0200

    Initial Commit

---
+++ Shared/OutlinePathApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutlinePathApp.swift
+//  Shared
+//
+//  Created by Łukasz Dziedzic on 26/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct OutlinePathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
